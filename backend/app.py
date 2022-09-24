da27c5f6fff7078e6be5719517e5ba4d1f5adada
Nicola Beirer <nicolabeirer12@gmail.com>
2022-09-24 04:08:13 -0400

    add gunicorn

--- backend/app.py
+++ backend/app.py
@@ -8,3 +8,6 @@ def boxing():
   combos = request.args.get('combos', type=int)
   rounds = request.args.get('rounds', type=int)
   return boxing_combination(combos, rounds)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000)
